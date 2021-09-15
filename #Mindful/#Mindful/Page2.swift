2e40e552b08c5df3749ce0481d979c8402784452
Mansi Gandhi <mansimg@gmail.com>
2021-09-15 14:58:25 -0400

    Add files via upload

---
+++ #Mindful/#Mindful/Page2.swift
@@ -0,0 +1,13 @@
+//
+//  Page2.swift
+//  #Mindful
+//
+//  Created by Donut on 3/23/19.
+//  Copyright © 2019 Donut. All rights reserved.
+//
+
+import UIKit
+
+class Page2: NSObject {
+
+}
---
+++ profilePage/profilePage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  profilePage
+//
+//  Created by Thrisha Kopula on 3/23/19.
+//  Copyright © 2019 Thrisha Kopula. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
